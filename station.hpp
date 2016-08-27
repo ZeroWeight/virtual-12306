bed45ae9dcf29680101e1a62db407f29a4ba404e
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-08-27 11:40:55 +0800

    station

---
+++ station.hpp
@@ -0,0 +1,43 @@
+/******************************************************************************************
+ * C++ Programming Task
+ * Project: A virtual 12306
+ * Weight Zero zwt15@mails.tsinghua.edu.cn
+ * Department of Automation, Tsinghua University
+ * est. time 2016-8-26
+ * "station.hpp"
+ * This file get the stations
+ ******************************************************************************************/
+#ifndef STATION_HPP
+#define STATION_HPP
+enum Station{
+    PEK=0,//beijing
+    CGO,//zhengzhou
+    CGQ,//changchun
+    CKG,//chongqing
+    CSX,//changsha
+    CTU,//chengdu
+    DLC,//dalian
+    FOC,//fuzhou
+    CAN,//guangzhou
+    KWL,//guilin
+    HAK,//haikou
+    HFE,//hefei
+    HGH,//hangzhou
+    HRB,//ha'erbin
+    JJN,//quanzhou
+    KHN,//nanchang
+    KMG,//kunming
+    NGB,//ningbo
+    NKG,//nanjing
+    SHA,//shanghai
+    SHE,//shenyang
+    SJW,//shijiazhuang
+    SZX,//shenzhen
+    URC,//wulumuqi
+    WUH,//wuhan
+    XIY,//xian
+    YTY,//yangzhou
+    ZUH,//zhuhai
+};
+
+#endif // STATION_HPP
