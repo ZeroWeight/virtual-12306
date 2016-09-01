aa187c1713d2a934e4de9f3e1da8291c1e6db6fd
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-09-01 10:40:15 +0800

    pay

--- temp/pay/pay.h
+++ temp/pay/pay.h
@@ -1,15 +1,57 @@
 #ifndef PAY_H
 #define PAY_H
-
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QWidget>
 #include <QMainWindow>
 
 class Pay : public QMainWindow
 {
     Q_OBJECT
-
+private:
+    QWidget *widget;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLineEdit *lineEdit;
+    QDialogButtonBox *buttonBox_2;
+    QWidget *widget_2;
+    QDialogButtonBox *buttonBox;
+    QWidget *formLayoutWidget_2;
+    QFormLayout *formLayout_2;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLineEdit *lineEdit_2;
+    QLineEdit *lineEdit_3;
+    QWidget *widget1;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer_2;
+    QPushButton *pushButton_2;
+    QSpacerItem *horizontalSpacer_3;
 public:
     Pay(QWidget *parent = 0);
     ~Pay();
+public slots:
+    void show();
 };
 
 #endif // PAY_H
